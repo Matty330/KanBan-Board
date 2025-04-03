1efb93806a650465ba9009f3f8db67f86ba81c76
Matthew Welcome <mwelc64@yahoo.com>
2025-04-03 10:50:11 -0400

    Update index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,7 +6,16 @@ import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        },
+        decimalNumbers: true,
+      },
+    })
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
